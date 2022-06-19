06c9db032b5759e1e9652b5f3d5b54631aaef4b7
hyoeun Kim <hyosaritech@gmail.com>
2022-06-19 22:54:09 +0900

    feat: Add getting start

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,12 @@
 module Test.MySolutions where
 
 import Prelude
+
+import Data.Int (rem)
+import Data.Number (sqrt, pi)
+
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea r = r * r * pi
+
+leftoverCents n = rem n 100
